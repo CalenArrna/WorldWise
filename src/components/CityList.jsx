a591d954c0236db6d1732f6b23d51ffb9a8b0b21
Nagy Csaba <nagycsaba@MacBook-Pro-16.local>
2024-01-21 19:57:26 +0100

    feat(app): implemented cities list tab, added json-server to mock backend

---
+++ src/components/CityList.jsx
@@ -0,0 +1,22 @@
+import CityItem from "./CityItem.jsx";
+import styles from "./CityList.module.css";
+import Spinner from "./Spinner.jsx";
+import Message from "./Message.jsx";
+
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first City, by clicking a city on the map!" />
+    );
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CityList;
